d6f2fb8c63c83ce17eb01d15eb70f4dcec51a968
zoulu <zoulu669064@live.com>
2017-05-20 21:44:14 +0800

    发布订阅demo

---
+++ RedisDemo/Common/RedisConnectionHelp.cs
@@ -0,0 +1,161 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+
+namespace Common
+{
+    /// <summary>
+    /// ConnectionMultiplexer对象管理帮助类
+    /// </summary>
+    public static class RedisConnectionHelp
+    {
+        /// <summary>
+        /// 系统自定义Key前缀
+        /// </summary>
+        public static readonly string SysCustomKey = ConfigurationManager.AppSettings["RedisKey"] ?? "";
+
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        private static readonly string RedisConnectionString = ConfigurationManager.AppSettings["RedisHosts"] ?? "127.0.0.1:6379";
+
+        private static readonly object Locker = new object();
+        private static ConnectionMultiplexer _instance;
+        /// <summary>
+        /// 线程安全的字典
+        /// </summary>
+        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> ConnectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
+
+        /// <summary>
+        /// 单例获取
+        /// </summary>
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Locker)
+                    {
+                        if (_instance == null || !_instance.IsConnected)
+                        {
+                            _instance = GetManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 缓存获取
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString)
+        {
+            if (!ConnectionCache.ContainsKey(connectionString))
+            {
+                ConnectionCache[connectionString] = GetManager(connectionString);
+            }
+            return ConnectionCache[connectionString];
+        }
+
+        /// <summary>
+        /// 获取连接  
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        private static ConnectionMultiplexer GetManager(string connectionString = null)
+        {
+            connectionString = connectionString ?? RedisConnectionString;
+            var connect = ConnectionMultiplexer.Connect(connectionString);
+
+            //注册如下事件
+            connect.ConnectionFailed += MuxerConnectionFailed;
+            connect.ConnectionRestored += MuxerConnectionRestored;
+            connect.ErrorMessage += MuxerErrorMessage;
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
+            connect.HashSlotMoved += MuxerHashSlotMoved;
+            connect.InternalError += MuxerInternalError;
+            connect.ConfigurationChangedBroadcast += MuxerConfigurationChangedBroadcast;
+
+            return connect;
+        }
+
+        #region 事件
+        /// <summary>
+        /// 重新配置广播时（通常意味着主从同步更改）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChangedBroadcast(object sender, EndPointEventArgs e)
+        {
+            Console.WriteLine($"{nameof(MuxerConfigurationChangedBroadcast)}: {e.EndPoint}");
+        }
+
+
+        /// <summary>
+        /// 配置更改时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+            Console.WriteLine("Configuration changed: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 发生内部错误时（主要用于调试）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            Console.WriteLine("ErrorMessage: " + e.Message);
+        }
+
+        /// <summary>
+        /// 重新建立连接之前的错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("ConnectionRestored: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 连接失败 ， 如果重新连接成功你将不会收到这个通知
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
+        }
+
+        /// <summary>
+        /// 更改集群时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+            Console.WriteLine("HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint);
+        }
+
+        /// <summary>
+        /// redis类库错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
+        {
+            Console.WriteLine("InternalError:Message" + e.Exception.Message);
+        }
+
+        #endregion
+    }
+}
---
+++ RedisDemo/PubSub/Program.cs
@@ -0,0 +1,54 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSub
+{
+    class Program
+    {
+        static RedisStackExchangeHelper _redis = new RedisStackExchangeHelper();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入发布订阅类型?1:发布；2：订阅");
+            var type = Console.ReadLine();
+            if (type == "1")
+            {
+                Pub();
+            }
+            else if(type=="2")
+            {
+                Sub();
+            }
+            while (true)
+            {
+
+            }
+        }
+
+        static async Task Pub()
+        {
+            Console.WriteLine("请输入要发布向哪个通道？");
+            var channel = Console.ReadLine();
+            while (true)
+            {    
+                Console.WriteLine("请输入要发布的消息内容.");
+                var message = Console.ReadLine();
+                await _redis.PublishAsync(channel, message);
+            }
+        }
+
+        static async Task Sub()
+        {
+            Console.WriteLine("请输入您要订阅哪个通道的信息？");
+            var channelKey = Console.ReadLine();
+            await _redis.SubscribeAsync(channelKey, (channel, message) =>
+            {
+                Console.WriteLine("接受到发布的内容为：" + message);
+            });
+            Console.WriteLine("您订阅的通道为：<< " + channelKey + " >> ! 请耐心等待消息的到来！！");
+        }
+    }
+}
