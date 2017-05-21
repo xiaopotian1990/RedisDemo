fe7cf9498946a8a1b36fa32f45e65189f5db3a0d
zoulu <zoulu669064@live.com>
2017-05-21 16:17:48 +0800

    分布式锁

---
+++ RedisDemo/Lock/Program.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lock
+{
+    class Program
+    {
+        static RedisStackExchangeHelper _redis = new RedisStackExchangeHelper();
+        static void Main(string[] args)
+        {
+            Lock();
+           
+        }
+
+        static void Lock()
+        {
+            Console.WriteLine("Start..........");
+            var db = _redis.GetDatabase();
+            RedisValue token = Environment.MachineName;
+            if (db.LockTake("test", token, TimeSpan.FromSeconds(10)))
+            {
+                try
+                {
+                    Console.WriteLine("Working..........");
+                    Thread.Sleep(5000);
+                }
+                finally
+                {
+                    db.LockRelease("test", token);
+                }
+            }
+
+            Console.WriteLine("Over..........");
+        }
+    }
+}
---
+++ RedisDemo/Transaction/Program.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Transaction
+{
+    class Program
+    {
+        static RedisStackExchangeHelper _redis = new RedisStackExchangeHelper();
+        static void Main(string[] args)
+        {
+            Tran();
+            Console.ReadLine();
+        }
+        static void Tran()
+        {
+            var tran = _redis.CreateTransaction();
+             tran.StringSetAsync("test1", "xiaopotian");
+             tran.StringSetAsync("test2", "xiaopangu");
+            var commit = tran.ExecuteAsync();
+            Console.WriteLine(commit);
+        }
+    }
+}
